14b61a6d12a252d7bcd72cb618d7c92ac3aa54ad
JStoker <jstoker@jcs.me.uk>
2010-12-14 04:18:22 +0000

    Refactored the srvsend functions to be part of the class, and modified the config not found error to not incorrectly warn if the config is not valid (SyntaxError, etc).

--- altara.py
+++ altara.py
@@ -1,7 +1,12 @@
 import asynchat,asyncore,socket,time
 
-class asynchat_bot(asynchat.async_chat):
-	def __init__(self, host, port):
+try:
+	import config
+except ImportError:
+	config = None
+
+class altara_socket(asynchat.async_chat):
+	def __init__(self, (host, port)):
 		asynchat.async_chat.__init__(self)
 		self.create_socket(socket.AF_INET,socket.SOCK_STREAM)
 		self.set_terminator('\r\n')
@@ -9,34 +14,36 @@ class asynchat_bot(asynchat.async_chat):
 		self.remote=(host,port)
 		self.connect(self.remote)
 	
+	def send(data):
+		print "Send: "+data
+		self.push(data+"\r\n")
+	
 	def handle_connect(self):
-		def srvsend(data):
-			print "Send: "+data
-			self.push(data+"\r\n")
-		srvsend("PASS "+config.linkpass+" TS 6 "+config.sid)
-		srvsend("CAPAB :QS EX IE KLN UNKLN ENCAP TB SERVICES EUID EOPMOD")
-		srvsend("SERVER "+config.servername+" 1 :"+config.serverdescription)
+		self.send("PASS "+config.linkpass+" TS 6 "+config.sid)
+		self.send("CAPAB :QS EX IE KLN UNKLN ENCAP TB SERVICES EUID EOPMOD")
+		self.send("SERVER "+config.servername+" 1 :"+config.serverdescription)
 	
 	def get_data(self):
 		r=self.data
 		self.data=''
 		return r
+	
 	def collect_incoming_data(self, data):
 		self.data+=data
+	
 	def found_terminator(self):
-		def srvsend(data):
-			print "Send: "+data
-			self.push(data+"\r\n")
 		data=self.get_data()
 		split = str(data).split(" ")
+		
 		print "Recv: "+data
+		
 		if split[0] == "PING":
-			srvsend("PONG "+split[1])
+			self.send("PONG "+split[1])
+
 if __name__ == '__main__':
-	try:
-		import config
-	except:
+	if config is None:
 		print "Please edit config.py.dist.  After you're done, rename it to config.py and try launching altara services again."
 		exit()
-	asynchat_bot(config.networkIP,config.linkport)
+
+	altara_socket((config.networkIP, config.linkport))
 	asyncore.loop()
