285efdf77a10a7c097a665ddcf187e7bfd703e88
jhfi <jhfi@outlook.com>
2014-08-13 03:47:50 -0400

    Update altara.py

--- altara.py
+++ altara.py
@@ -1,16 +1,16 @@
-	# This code is licensed under the AGPL v3 license.
-	# 
-    #~ Copyright (C) 2011  Jason Gates (bikcmp) and contributors.
-    #~ This program is free software: you can redistribute it and/or modify
-    #~ it under the terms of the GNU Affero General Public License as
-    #~ published by the Free Software Foundation, either version 3 of the
-    #~ License, or (at your option) any later version.
-    #~ This program is distributed in the hope that it will be useful,
-    #~ but WITHOUT ANY WARRANTY; without even the implied warranty of
-    #~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    #~ GNU Affero General Public License for more details.
-    #~ You should have received a copy of the GNU Affero General Public License
-    #~ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This code is licensed under the AGPL v3 license.
+# 
+#~ Copyright (C) 2011  Jason H. and contributors.
+#~ This program is free software: you can redistribute it and/or modify
+#~ it under the terms of the GNU Affero General Public License as
+#~ published by the Free Software Foundation, either version 3 of the
+#~ License, or (at your option) any later version.
+#~ This program is distributed in the hope that it will be useful,
+#~ but WITHOUT ANY WARRANTY; without even the implied warranty of
+#~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#~ GNU Affero General Public License for more details.
+#~ You should have received a copy of the GNU Affero General Public License
+#~ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #Note: before you do anything with this in production, DISABLE D-EXEC!
 
 
@@ -139,7 +139,6 @@ class altara_socket(asynchat.async_chat):
 				self.firstSync = 0
 		elif split[1] == "EUID":
 			#Recv: :05K EUID jason 3 1292805989 +i ~jason nat/session 0 05KAAANCY * * :Jason
-			#Note: could use some cleanup here.
 			modes = split[5].strip("+").strip("-")
 			nick = split[2]
 			user = split[6]
